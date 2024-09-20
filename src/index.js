c04dfa395439b972898ae0dc4d269bf8a43c4170
gabencityyy97 <gabrielatsvyatkova51@gmail.com>
2024-09-20 14:54:24 +0300

    Add homeController

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 
+import homeController from './controllers/homeController.js'
 
 const app = express();
 
@@ -12,8 +13,7 @@ app.set('views', './src/views');
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) => {
-    res.render('home')
-});
+app.use(homeController);
+
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'))
