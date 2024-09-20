baf457c5e1442e3595f1e98eab1adc981da1f0d7
gabencityyy97 <gabrielatsvyatkova51@gmail.com>
2024-09-20 15:09:14 +0300

    Add routes module

---
+++ src/router.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import homeController from './controllers/homeController.js';
+
+
+const router = Router();
+
+router.use(homeController);
+
+export default router;
