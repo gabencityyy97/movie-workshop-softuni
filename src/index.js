60f021ea3b86223561b0b4d834ec67ecffedd058
gabencityyy97 <gabrielatsvyatkova51@gmail.com>
2024-09-20 14:07:31 +0300

    set up express

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import express from 'express';
 
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Just works!')
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'))
