4e2f770b53c8eadbaf59be6d2183c0d4415476df
gabencityyy97 <gabrielatsvyatkova51@gmail.com>
2024-09-22 15:59:18 +0300

    Create movie functionallity

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import movieServices from "../services/movieServices.js";
 
 const router = Router();
 
 router.get('/create', (req,res) => {
     res.render('movies/create')
 });
+
+router.post('/create', async (req, res) => {
+    const movieData = req.body;
+
+   await  movieServices.create(movieData)
+    res.end();
+})
 
 export default router;
